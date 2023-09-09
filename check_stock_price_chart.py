43e97b80a3822596a58c4fc93d8c2f2b8750f319
hk29-ai <64739845+hk29-ai@users.noreply.github.com>
2023-09-09 17:20:37 +0900

    Update check_stock_price_chart.py

--- check_stock_price_chart.py
+++ check_stock_price_chart.py
@@ -3,17 +3,26 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pandas_datareader.data as web
-from dateutil import relativedelta
+
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import japanize_matplotlib
 plt.rcParams['font.size'] = 24 # グラフの基本フォントサイズの設定
+
 from prophet import Prophet
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+from keras import optimizers
+
 import datetime as dt
+from dateutil import relativedelta
+from datetime import timedelta
+
 now = dt.datetime.now() # 今日の日付を取得する
 st.write(now)
 
-###################################################### データの読み込み
+###################################################### 1. 株価データの読み込み
 # 東証上場銘柄一覧データを読み込む
 df = pd.read_csv('data_j.csv')
 
@@ -25,7 +34,7 @@ random_No = str(random.randint(1, N))
 data_df = df.iloc[int(random_No)]
 
 st.write("""
-### ■ランダムに選択した株の銘柄
+### ■1. ランダムに選択した株の銘柄
 """)
 st.table(data_df)
 
@@ -48,7 +57,7 @@ df['Ave'] = (df['Open'] + df['High'] + df['Low'] + df['Close']) / 4
 # インデックスでソートする
 DF = df.sort_index(ascending = True)
 
-##################################################### データ処理
+##################################################### 2. チャートとかかくたいの出来高
 # 価格帯の最小と最大を算出
 min = int(DF['Ave'].min())
 max = int(DF['Ave'].max())
@@ -72,25 +81,7 @@ simple_moving_average1 = pd.Series.rolling(DF['Close'], window=my_days1).mean()
 simple_moving_average2 = pd.Series.rolling(DF['Close'], window=my_days2).mean()
 simple_moving_average3 = pd.Series.rolling(DF['Close'], window=my_days3).mean()
 
-##################################################### Prophetによる予測モデルの作成
-DF2 = DF.copy()
-DF2['ds'] = DF2.index # 日付がインデックスにあるため、列にする
-DF2 = DF2.rename(columns={'Close': 'y'}) # 目的変数の名前をyに置換する
-
-# モデルの作成
-model = Prophet(
-    growth='linear', # 傾向変動の関数．非線形は'logistic'
-    yearly_seasonality = True, # 年次の季節変動を考慮有無
-    weekly_seasonality = False, # 週次の季節変動を考慮有無
-    daily_seasonality = False, # 日次の季節変動を考慮有無
-    changepoints = None, #  傾向変化点のリスト
-    changepoint_range = 0.85, # 傾向変化点の候補の幅で先頭からの割合。
-    changepoint_prior_scale = 0.5, # 傾向変化点の事前分布のスケール値。パラメータの柔軟性
-    n_changepoints = 5, # 傾向変化点の数
-) 
-model.fit(DF2)
-
-###################################################### データの可視化
+### データの可視化
 fig = plt.figure(figsize=(21,9))
 
 # 株価チャートのグラフを描く
@@ -108,7 +99,7 @@ plt.setp(x_ticklabels, rotation=75) # 目盛り表記を90度回転。#フォン
 tick_spacing = 180 # 目盛り表示する間隔(3か月=90日)
 ax1.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing)) # X軸目盛の表示間隔を間引く
 ax1.grid()
-ax1.set_title(bland)
+ax1.set_title(f'{bland}：{ticker_symbol}')
 
 # 価格帯別の出来高のグラフを描く
 ax2 = fig.add_axes([0.72, 0.1, 0.15, 0.8])
@@ -116,15 +107,34 @@ ax2.barh(label_list, my_sum['Volume'], color="g")
 ax2.set_xlabel('出来高')
 ax2.set_ylabel('価格帯')
 st.write("""
-### ■チャートと価格帯別の出来高
+### ■2. チャートと価格帯別の出来高
 下図左は株価チャートで、移動平均線を計算してプロットしています。  下図右は出来高を価格帯別に算出して図示しています。
 """)
 st.pyplot(fig)
 
-##### Prophetによる株価予測を図示
+
+##################################################### 3. Prophetによる予測
+DF2 = DF.copy()
+DF2['ds'] = DF2.index # 日付がインデックスにあるため、列にする
+DF2 = DF2.rename(columns={'Close': 'y'}) # 目的変数の名前をyに置換する
+
+# モデルの作成
+model = Prophet(
+    growth='linear', # 傾向変動の関数．非線形は'logistic'
+    yearly_seasonality = True, # 年次の季節変動を考慮有無
+    weekly_seasonality = False, # 週次の季節変動を考慮有無
+    daily_seasonality = False, # 日次の季節変動を考慮有無
+    changepoints = None, #  傾向変化点のリスト
+    changepoint_range = 0.85, # 傾向変化点の候補の幅で先頭からの割合。
+    changepoint_prior_scale = 0.5, # 傾向変化点の事前分布のスケール値。パラメータの柔軟性
+    n_changepoints = 5, # 傾向変化点の数
+) 
+model.fit(DF2)
+
+#### データの可視化
 st.write("""
-### ■Prophetを用いた株価予測
-時系列データ分析のPythonライブラリ「Prophet」による株価予測です。下図中の右端の黒点がないあたりが予測結果です。一般的に株価の予測については、これに限らず鵜呑みにしないように注意ください。予測するにもハイパーパラメータの他に外的要因もあるためです。
+### ■3. Prophetを用いた株価予測
+時系列データ分析のPythonライブラリ「Prophet」による株価予測です。下図中の右端の黒点がないあたりが予測結果です。
 """)
 
 # 学習データに予測したい期間を追加する
@@ -151,3 +161,121 @@ ax.set_xlabel('')
 ax.set_ylabel('株価 [円]')
 
 st.pyplot(fig)
+
+##################################################### RNNによる予測
+df_close = DF["Close"] # 終値のみ使用
+
+# データの正規化
+max_value = df_close.max()
+min_value = df_close.min()
+df_close_normalize = (df_close - min_value) / (max_value - min_value)
+
+# 学習用とテスト用に分割
+train_size = int(len(df_close_normalize) * 0.75) # 学習用データの割合
+train = df_close_normalize[:train_size]
+test = df_close_normalize[train_size:]
+
+# 入出力を作成する関数
+def create_dataset(_data, _window_size):
+    X, y = [], []
+    for i in range(len(_data) - _window_size):
+        X.append(_data[i:i + _window_size])
+        y.append(_data[i + _window_size])
+    return np.array(X), np.array(y)
+
+window_size = 14 # 学習データの日にち区間
+X_train, y_train = create_dataset(train, window_size)
+X_test, y_test = create_dataset(test, window_size)
+
+# LSTMの入力形式へ変換
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+
+# モデルの構築
+model = Sequential()
+model.add(LSTM(50, activation='relu', return_sequences=True, input_shape=(window_size, 1)))
+model.add(LSTM(50, activation='relu', return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# オプティマイザーの設定
+my_optimizer = optimizers.Adam(
+    learning_rate=0.001,
+    beta_1=0.9,
+    beta_2=0.999,
+    epsilon=1e-07,
+    amsgrad=False,
+    weight_decay=None,
+)
+
+# モデルのコンパイル
+model.compile(optimizer = my_optimizer, loss = "mean_squared_error")
+
+# モデルの学習
+model.fit(X_train, y_train, epochs=10, batch_size=32) # エポック数, バッチサイズ
+
+# モデルの評価
+train_score = model.evaluate(X_train, y_train)
+test_score = model.evaluate(X_test, y_test)
+st.write("Train Score: {:.4f}".format(train_score))
+st.write("Test Score: {:.4f}".format(test_score))
+
+# 日付データの取得
+dates = df_close_normalize.index # 日付データがインデックスのため、取得
+dates = pd.to_datetime(dates) # 日付データをdatetime型に変換
+
+# 訓練用データの日付データの取得
+train_dates = dates[:train_size]
+# テスト用データの日付データの取得
+test_dates = dates[train_size + window_size:] 
+
+# 予測値の取得
+y_pred = model.predict(X_test)
+# 予測値の逆正規化
+y_pred = y_pred * (max_value - min_value) + min_value
+
+# 未来の1か月分の日付データの作成
+pred_days = 90 # 予測したい日数
+
+last_date = test_dates[-1] # 最後の日付を取得
+future_dates = [last_date + timedelta(days=i) for i in range(1, pred_days + 1)] # 最後の日付から1日ずつ増やしてリストに追加
+future_dates = pd.to_datetime(future_dates) # 日付データをdatetime型に変換
+
+# 未来の予測値の取得
+future_pred = []
+last_input = X_test[-1] # 最後の入力データを取得
+
+# 予測したい日数分実行する
+for i in range(pred_days):
+    pred = model.predict(last_input.reshape(1, window_size, 1)) # 入力データから予測値を取得
+    future_pred.append(pred[0][0]) # 予測値をリストに追加
+    last_input = np.append(last_input[1:], pred) # 入力データを更新
+
+# 未来の予測値の逆正規化
+future_pred = np.array(future_pred) * (max_value - min_value) + min_value
+
+### データの可視化
+st.write("""
+### ■4. RNNによる株価予測
+Pythonライブラリ「keras」を用いて、RNNの1種であるLSTM（Long Short-Term Memory）による株価予測です。下図中の右端のピンクが未来予測です。
+""")
+fig, ax = plt.subplots(figsize=(10,6))
+# 訓練データ
+plt.plot(train_dates, train * (max_value - min_value) + min_value, label="Train")
+# テストデータ
+plt.plot(test_dates, test[window_size:] * (max_value - min_value) + min_value, label="Actual")
+# テストデータ部分の予測
+plt.plot(test_dates, y_pred, label="Predicted")
+# 未来の予測
+plt.scatter(future_dates, future_pred, label="Future", marker='o', linewidth=3, color='magenta')
+
+plt.title(f'{bland}：{ticker_symbol}')
+plt.ylabel('株価 [円]')
+plt.legend()
+plt.xticks(rotation=45)
+st.pyplot(fig)
+
+st.write("""
+※一般的に株価の予測については、これらに限らず鵜呑みにしないように注意ください。
+結局、株価の変動は外的要因とそれによって生じる心理的要因が大きいと思います
+""")
